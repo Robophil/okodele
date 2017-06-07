f92ddda99165d3a248baaac4db673326d0ab6957
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-07 07:12:04 +0100

    test start server. It works

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 const BootBot = require('bootbot')
 const config = require('config')
 
+//create new instant of bot
 const bot = new BootBot({
   accessToken: config.get("pageAccessToken"),
   verifyToken: config.get("validationToken"),
   appSecret: config.get("appSecret")
 })
+
+
+bot.on('message', (payload, chat) => {
+	const text = payload.message.text;
+	console.log(`The user said: ${text}`)
+})
+
+bot.start()
