a7bf3fd3f1cf404ab9ae96e15f56c622e29e2d10
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-07 06:47:19 +0100

    used config data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const BootBot = require('bootbot')
 const config = require('config')
 
 const bot = new BootBot({
-  accessToken: 'FB_ACCESS_TOKEN',
-  verifyToken: 'FB_VERIFY_TOKEN',
-  appSecret: 'FB_APP_SECRET'
+  accessToken: config.get("pageAccessToken"),
+  verifyToken: config.get("validationToken"),
+  appSecret: config.get("appSecret")
 })
